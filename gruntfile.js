bf996725d25ce7e89fb144df53632a31306e9984
Markus Falk <mail@markus-falk.com>
2015-08-09 11:12:32 +0200

    [TASK] add jsonlint

---
+++ gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jsonlint: {
+      mfcc: {
+        src: ['json/*.json']
+      }
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-jsonlint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jsonlint']);
+
+};
